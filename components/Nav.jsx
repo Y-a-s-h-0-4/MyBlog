36cf238dda5bb0487de6f8e3560e1f0e2a2c80c9
Yash <Yashwanthpothala04@gmail.com>
2024-04-18 07:26:37 +0530

    Add useInsertionEffect hook to Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,16 +1,46 @@
 'use client';
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useInsertionEffect } from 'react'
 import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
 const Nav = () => {
+  const isUserLoggedIn = true;
+  const [Providers, setProviders] = useState(null);
+  useEffect(() => {
+const setProviders = async ()=>{
+  const response = await getProviders()
+}
+}, [])
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href={"/"} className="flex gap-2 flex-center">
         <Image src={"/assets/images/logo.svg"} alt="logo" width={30} height={30}/>
         <p className="logo_text">My Blog</p>
 
-        {/* Mobile Navigation */}
+        {/* Desktop Navigation */}
+        <div className="sm:flex hidden">
+          {isUserLoggedIn ? (
+            <div className='flex gap-3 md:gap-5'>
+              <Link href={"/create-post"} className={"black_btn"}>
+              Create Post
+              </Link>
+              <button type='button' onClick={() => signOut()} className={"outline_btn"}>
+                Sign Out
+              </button>
+
+              <Link href={"/profile"} className={"black_btn"}>
+                <Image src={"/assets/images/profile.png"} alt="profile" width={37} height={37}/>
+              </Link>
+            </div>
+          ) : (
+            <>
+            
+            </>
+          )
+          }
+
+        </div>
       </Link>
     </nav>
 
